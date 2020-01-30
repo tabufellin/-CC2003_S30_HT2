6da6828a21f20d393cbf2d40316bebb01a2c09cc
Tabufell <dsgspersonal@gmail.com>
2020-01-30 15:14:13 -0600

    Hice el main, ya estoy haciendo el metodo que extrae el archivo de texto pero no esta terminado, agregue el main y un .txt de prueba

--- GuatemalanCalculator.java
+++ GuatemalanCalculator.java
@@ -1,13 +1,29 @@
 import java.lang.invoke.SwitchPoint;
 import java.util.ArrayList;
+import java.io.*;
 
 public class GuatemalanCalculator {
     private QuetzalStack<Integer> resultStack = new QuetzalStack<Integer>();
-    ArrayList<String> importText() {
-        return //Document data
+
+    public ArrayList<String> importText() {
+
+        String direccion = "C:/Users/dsgsp/OneDrive/Documentos/progra/Estructuras/semana3/3/-CC2003_S30_HT2/texto.txt";
+        String datosDocumento = leerTxt(direccion);
+        String datosEnArray[] = datosDocumento.split(" ");
+        ArrayList<String> datosEnArrayList = new ArrayList();
+        for (int i = 0; i >= datosEnArray.length; i++) {
+            datosEnArrayList.add(datosEnArray[i]);
+            System.out.println(datosEnArrayList.get(1));
+        }
+        //System.out.println(datosEnArray.length);
+        
+        return datosEnArrayList;
     }
 
     boolean validateText(String line) {
+
+        // incompleto
+
         return true;
     }
 
@@ -26,6 +42,37 @@ public class GuatemalanCalculator {
         }
 
         return result;
+    }
+
+    public String leerTxt (String direccion ) {
+
+        String texto = "";
+
+
+
+
+        try {
+
+            BufferedReader bf = new BufferedReader(new FileReader(direccion));
+            String temp = "";
+            String bfRead;
+
+            while((bfRead = bf.readLine()) != null) {
+
+                temp = temp + bfRead; // aquí lo guarda en temp
+            }
+
+            texto = temp;
+
+
+
+        } catch (Exception e) {
+            System.err.println("No se encontro un archivo");
+
+        }
+
+        return texto;
+
     }
 
 }
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args) {
+
+        GuatemalanCalculator myCalculator = new GuatemalanCalculator();
+        System.out.println(myCalculator.importText());
+
+    }
+}
