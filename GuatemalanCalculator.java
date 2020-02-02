941d21855e05b4edf901fe8dc050b23d6fb7f987
PingMaster99 <59905458+PingMaster99@users.noreply.github.com>
2020-02-01 20:50:22 -0600

    Método calculate terminado
    Utiliza un switch dentro de un for para evaluar la operación a realizar según el caracter.

--- GuatemalanCalculator.java
+++ GuatemalanCalculator.java
@@ -1,6 +1,6 @@
-import java.lang.invoke.SwitchPoint;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
-import java.io.*;
 
 public class GuatemalanCalculator {
     private QuetzalStack<Integer> resultStack = new QuetzalStack<Integer>();
@@ -56,20 +56,46 @@ public class GuatemalanCalculator {
     }
 
     int calculate(String line) {
-
+        int firstNumber = 0;
+        int secondNumber = 0;
         int result = -1;
-        
+
         for(int i = 0; i < line.length(); i++) {
             char character = line.charAt(i);
             switch (character) {
                 case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
                     int number = Character.getNumericValue(character);
-                    // hacer push en el stack
+                    resultStack.push(number);
+                    break;
 
+                case '+':
+                    firstNumber = resultStack.pop();
+                    secondNumber = resultStack.pop();
+                    resultStack.push(firstNumber + secondNumber);
+                    break;
+
+                case '-':
+                    firstNumber = resultStack.pop();
+                    secondNumber = resultStack.pop();
+                    resultStack.push(firstNumber - secondNumber);
+                    break;
+
+                case '*':
+                    firstNumber = resultStack.pop();
+                    secondNumber = resultStack.pop();
+                    resultStack.push(firstNumber * secondNumber);
+                    break;
+
+                case '/':
+                    firstNumber = resultStack.pop();
+                    secondNumber = resultStack.pop();
+                    resultStack.push(firstNumber / secondNumber);
+                    break;
+
+                default:
+                    return -1000101;    // Error code (binary E) used to signify an invalid line calculation. 
             }
-
         }
-
         return result;
     }
 
